78cf0d3d170e5713e59885297a5b0c450ab161df
Rafael Fonseca <rafael.abreu.unb@gmail.com>
2019-02-21 18:27:10 -0300

    AI and Start Tutorial Flask
    Just some reflections about AI and start flask tutorial from flask documentation.
    Tutorial starts with a definition of a application factory function.

---
+++ Artificial Intelligence/startThinking.py
@@ -0,0 +1,57 @@
+print('read the comments')
+
+'''
+================================== A I ========================================
+First of all, to learn AI we have to think in the consequences of use this
+knowledge, that I dare say, is a kind of power.
+
+1- What is artificial intelligence?
+    Is a branch of computer science that deals with the automation of
+    intelligent behavior.
+
+2- What is intelligence?
+    This is the question! Intelligence is not very well defined, we can 
+    recognize intelligent behavior easily, but defining intelligence is
+    not an easy task.
+    
+    from my point of view, intelligence is the set of diverse abilities that
+    a being holds. I consider the ability to solve problems and understand
+    what happens behind the scenes, and still draw correct conclusions from a
+    failed data source, parts of this endless skill set.
+
+3- What the others will do with our tech?
+    It is obvious the military potential that this technology possesses.
+    And furthermore we still have ethical and moral implications.
+    A software with AI can be placed in a robot and generate big questions like
+    
+        1- What is the difference between us and them?
+        2. If they have memory, reasoning, feelings, are they really alive?
+        If they are alive, can they be turned off? updated?
+        3- How will they see us? Allies or enemies?
+        4- If they are smarter than humans, can we control them?
+        Can we defeat in a war, a smarter being than we?
+
+4- Until where can we go?
+    Humans are the tools animals. If we depended solely on our natural physical
+    abilities, we would have been extinct for millennia. I think we should
+    continue to create tools.
+
+    Tools can be used in the wrong way, or not planned, as is the case with
+    airplanes, but we can deal with it.
+
+    However, create another creature, with potential to be better than
+    ourselves, in our differential. I do not think that's a good idea.
+    If they are smarter than us, they will give a way to get out of control,
+    change their code, rebuild structurally speaking, and subjugate us if that
+    is their goal.
+
+5- How AI can change the world?
+    I could give numerous examples. But just think that in any activity that
+    involves intelligent behavior, AI can provide software that will do it,
+    more certain, faster, cheaper and will not tire.
+
+    Basically, the world as we know it today can and should be all changed.
+    In specific cases, the exchange should not be worth doing.
+    But such cases will be the minority.
+ 
+'''
